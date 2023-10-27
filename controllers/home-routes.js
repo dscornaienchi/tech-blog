edccac5c2ef851f1ab331bf18d372295cf649f4a
dscornaienchi <Dscorni32@gmail.com>
2023-10-27 13:38:40 -0500

    started api routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [{model: User}, {model: Comment}],
+        });
+
+        const posts = postData.map((post) => post.get({plain: true}));
+
+        res.render('home', {posts});
+    }   catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
