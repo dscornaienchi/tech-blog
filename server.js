9113a1bf951d0181cc277cf99a6201ef88f8e8b8
dscornaienchi <Dscorni32@gmail.com>
2023-10-27 13:31:29 -0500

    setup server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+
+app.use(routes);
+
+sequelize.sync({force:false}).then(() => {
+    app.listen(PORT, () => console.log('Server is now running on http://localhost:${PORT}'));
+});
