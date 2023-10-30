6b1e3e2894d7c8e0e0a61d956c096224a36d3248
dscornaienchi <Dscorni32@gmail.com>
2023-10-30 16:56:04 -0500

    added more routes to home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,133 @@
 const router = require('express').Router();
-const { Post, User, Comment } = require('../models');
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
 
+// Get all posts and render the homepage
 router.get('/', async (req, res) => {
-    try {
-        const postData = await Post.findAll({
-            include: [{model: User}, {model: Comment}],
-        });
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User }],
+    });
 
-        const posts = postData.map((post) => post.get({plain: true}));
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-        res.render('home', {posts});
-    }   catch (err) {
-        console.error(err);
-        res.status(500).json(err);
-    }
+    res.render('home', {
+      posts,
+      user: req.session.user_id,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
+
+// View a single post
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: User }],
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    const post = postData.get({ plain: true });
+
+    res.render('post', { post, user: req.session.user_id });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// Access the create post page (requires authentication)
+router.get('/create', withAuth, (req, res) => {
+  res.render('create-post', { user: req.session.user_id });
+});
+
+// Create a new post
+router.post('/create', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// Access the update post page (requires authentication)
+router.get('/update/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    const post = postData.get({ plain: true });
+
+    res.render('update-post', { post, user: req.session.user_id });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// Update a post
+router.put('/update/:id', withAuth, async (req, res) => {
+  try {
+    const updatedPost = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (updatedPost[0] === 0) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Post updated successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// Delete a post
+router.delete('/delete/:id', withAuth, async (req, res) => {
+  try {
+    const deletedPost = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedPost) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Post deleted successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
