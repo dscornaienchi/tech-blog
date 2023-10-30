9e6bb846371d720a8c03982f51948ab67aadb028
dscornaienchi <Dscorni32@gmail.com>
2023-10-30 18:43:16 -0500

    updates to routes for debugging

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 
-
 // User registration route
 router.post('/register', async (req, res) => {
   try {
