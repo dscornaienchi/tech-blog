58a0b074f866a5e109c3def3a5d573510a72dc51
dscornaienchi <Dscorni32@gmail.com>
2023-10-30 17:05:12 -0500

    updated api routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 // Get all posts
 router.get('/', async (req, res) => {
@@ -38,6 +39,87 @@ router.get('/post/:id', async (req, res) => {
   }
 });
 
-// Add routes for creating, updating, and deleting posts
+// Create a new post (requires authentication)
+router.post('/create', withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      title: req.body.title,
+      content: req.body.content,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newPost);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// Access the update post page (requires authentication)
+router.get('/update/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    const post = postData.get({ plain: true });
+
+    res.render('update-post', { post, user: req.session.user_id });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// Update a post (requires authentication)
+router.put('/update/:id', withAuth, async (req, res) => {
+  try {
+    const updatedPost = await Post.update(
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (updatedPost[0] === 0) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Post updated successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// Delete a post (requires authentication)
+router.delete('/delete/:id', withAuth, async (req, res) => {
+  try {
+    const deletedPost = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!deletedPost) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    res.status(200).json({ message: 'Post deleted successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,19 +1,19 @@
 const router = require('express').Router();
 const { User } = require('../models');
 
-// User registration route
+// Register a new user
 router.post('/register', async (req, res) => {
   try {
     const userData = await User.create({
-      username: req.body.username,
+      email: req.body.email,
       password: req.body.password,
     });
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.username = userData.username;
       req.session.logged_in = true;
-      res.json({ user: userData, message: 'Registration successful!' });
+
+      res.status(201).json(userData);
     });
   } catch (err) {
     console.error(err);
@@ -21,6 +21,48 @@ router.post('/register', async (req, res) => {
   }
 });
 
-// Add other user routes, e.g., login and logout
+// Login route
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+
+// Logout route
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
