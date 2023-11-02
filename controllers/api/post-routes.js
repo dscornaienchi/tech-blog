5c580ecc69886a12e525fabf568427e4005dc0fb
dscornaienchi <Dscorni32@gmail.com>
2023-11-01 19:42:49 -0500

    debugged server error

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../models');
+const { User, Post, Comment } = require('../../models');
 
 // Get all posts
 router.get('/', async (req, res) => {
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../models');
+const { User, Post, Comment } = require('../../models');
 
 // User registration route
 router.post('/register', async (req, res) => {
